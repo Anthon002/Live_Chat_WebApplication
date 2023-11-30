18d2e7c598d5e029cccbdd2a656a2bf744b24ef5
Anthon002 <chineduanulugwo@gmail.com>
2023-11-30 07:16:07 +0100

    added first look at socket programming

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 from forms.authenticationForm import Registration, Login
 from uuid import uuid4
 from flask_bcrypt import Bcrypt
+from flask_socketio import SocketIO, send, emit
 
 app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins = "*")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 app.config["SECRET_KEY"] = str(uuid4())
 bcrypt = Bcrypt(app)
@@ -14,12 +16,32 @@ login_manager.init_app(app)
 login_manager.login_view ="login"
 db = SQLAlchemy(app)
 
-
+authUsername = None
+authId = None
+class AuthUser():
+    def __init__(self, username, id):
+        '''
+            this class serves as the class that will hold user value and carry it over to the template
+        '''
+        self.username = username
+        self.id = id
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(50), nullable=False)
+
+def authenticatin(newValueusername, newValueId):
+    '''
+        This function collects the authenticated users username and id by changing the globale variables authUsername and authId
+        Arguments:
+            newValueusername(string) : collects username
+            newValueId(string) : collects id
+    '''
+    global authUsername
+    global authId
+    authUsername = newValueusername
+    authId = newValueId
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -49,6 +71,7 @@ def login():
     passWord = form.Password.data
     if request.method and form.validate_on_submit():
        user = db.session.query(User).filter_by(username = userName).first()
+       authenticatin(user.username, user.id) #The global variables have been changed
        if user is not None:
            checkPassword = bcrypt.check_password_hash(user.password, passWord)
            if checkPassword:
@@ -60,10 +83,23 @@ def login():
 
 @app.route("/home")
 def home():
+    user = None
     if current_user.is_authenticated is False:
-        return redirect(url_for("registration"))
-    
-    return render_template("home.html")
+        return redirect(url_for("login"))
+    authUser = AuthUser(authUsername,authId)
+    return render_template("home.html", current_user=current_user, authUser = authUser)
+
+@socketio.on('connect')
+def handle_connect():
+    authUser = AuthUser(authUsername, authId)
+    print(authUser.username + "User Connected")
+
+@socketio.on('message')
+def handle_messages(message):
+    print("Message Recieved :" + message)
+    user = AuthUser(authUsername, authId)
+    emit("messageSendBack",{"message": message, "username": user.username}, broadcast= True )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    #app.run(debug=True)
+    socketio.run(app , host="localhost", debug = True)
